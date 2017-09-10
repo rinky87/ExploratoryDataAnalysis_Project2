67bb41123ea7dd834db5ba64d2a9cdbcc66d708e
rinky87 <31304630+rinky87@users.noreply.github.com>
2017-09-10 15:09:53 +0530

    Update Plot5.R

--- Plot5.R
+++ Plot5.R
@@ -1,6 +1,7 @@
 NEI<-readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 NEIBaltimore<-subset(NEI,NEI$fips=="24510")
+NEIBaltimoreMotor<-subset(NEIBaltimore,NEIBaltimore$type=="ON-ROAD"
 pm25TotalByYear<-aggregate(Emissions~year,NEIBaltimoreMotor,sum)
 png("Plot5.png")
 barplot(pm25TotalByYear$Emissions,names.arg=pm25TotalByYear$year,main="Total Emission from Motor Vehicle Baltimore by Year",xlab="Year",ylab="PM2.5 Emission")
